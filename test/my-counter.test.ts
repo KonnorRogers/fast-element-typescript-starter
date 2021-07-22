8a4fb051fa7542e68d6effe3341140ce87b223f9
ParamagicDev <konnor5456@gmail.com>
2021-07-22 05:59:40 -0400

    fix tests

--- test/my-counter.test.ts
+++ test/my-counter.test.ts
@@ -1,36 +1,32 @@
-// import { assert } from '@open-wc/testing'
-// import { fixture } from './helpers'
+import { fixture, assert } from '@open-wc/testing'
 
 // if this gets changed to `import "../src/my-counter` everything works.
 import { MyCounter } from '../src/my-counter'
 
+const tag = new MyCounter().tagName
+
 describe('MyCounter', () => {
   it('increases the count on button click', async () => {
-    // Uncomment below, "button" / shadowRoot will be defined.
-    // console.log(MyCounter)
-    const container = document.createElement('div')
-    container.dataset.testId = 'test-container'
-    container.innerHTML = '<my-counter></my-counter>'
-    document.body.appendChild(container)
-
-    const el = container.firstElementChild as MyCounter
-    const button = el?.shadowRoot?.querySelector('button[title="increment"]') as HTMLButtonElement
+    const el = await fixture<MyCounter>(`<${tag}></${tag}>`)
+    const button = el.shadowRoot?.querySelector('button[title="increment"]') as HTMLButtonElement
     button.click()
+    assert.equal(el.count, 1)
   })
 
-  // it('decreases the count on button click', async () => {
-  //   const el = await fixture<MyCounter>('<my-counter></my-counter>')
-  //   const button = el.shadowRoot?.querySelector('button[title="decrement"]') as HTMLButtonElement
-  //   button.click()
+  it('decreases the count on button click', async () => {
+    const el = await fixture<MyCounter>(`<${tag}></${tag}>`)
+    const button = el.shadowRoot?.querySelector('button[title="decrement"]') as HTMLButtonElement
+    button.click()
 
-  //   assert.equal(el.count, -1)
-  // })
+    assert.equal(el.count, -1)
+  })
 
-  // it('resets to 0', async () => {
-  //   const el = await fixture<MyCounter>('<my-counter count="5"></my-counter>')
-  //   assert.equal(el.count, 5)
-  //   const button = el.shadowRoot?.querySelector('button[title="reset"]') as HTMLButtonElement
-  //   button.click()
-  //   assert.equal(el.count, 0)
-  // })
+  it('resets to 0', async () => {
+    const el = await fixture<MyCounter>(`<${tag} count="5"></${tag}>`)
+    assert.equal(el.count, 5)
+
+    const button = el.shadowRoot?.querySelector('button[title="reset"]') as HTMLButtonElement
+    button.click()
+    assert.equal(el.count, 0)
+  })
 })
